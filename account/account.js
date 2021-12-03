678ddef6c293d6621a0b30344864f0fc86a2ec44
Garrison Parrish <tpgarris@live.unc.edu>
2021-12-03 12:52:38 -0500

    functionality for high score, deleted account.css

--- account/account.js
+++ account/account.js
@@ -2,4 +2,17 @@
 
 window.addEventListener("load", function(event) {
     document.getElementById("username").innerHTML = localStorage.getItem("username");
+    // Dont' Starve stats
+    // on load, update stat for dontStarveGamesPlayed
+    /*
+    const dontStarveGamesPlayed = document.getElementById("dontStarveGamesPlayed");
+    dontStarveGamesPlayed.innerHTML = this.localStorage.getItem("")
+    */
+    const dontStarveHighScore = document.getElementById("dontStarveHighScore");
+    dontStarveHighScore.innerHTML = this.localStorage.getItem("starveHighScore");
+
+    // Obstacle Course stats
+    const obstacleHighScore = document.getElementById("obstacleHighScore");
+    obstacleHighScore.innerHTML = this.localStorage.getItem("obstacleHighScore");
+
 });
